691af6a9c7189385742aef80e8d779768693ebab
Arnab Kumar Ghosh <arnab@Arnabs-MacBook-Pro-2.local>
2026-05-03 16:16:22 +0200

    change the heading of the page

--- A3/model_serving/single_server_without_docker/production_server/app.py
+++ A3/model_serving/single_server_without_docker/production_server/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Welcome to the Machine Learning Course.</h1>'
+    return '<h1>Welcome to the Machine Learning Course. This is the production without Docker.</h1>'
 
 @app.route("/accuracy", methods=['POST', 'GET'])
 def accuracy():
