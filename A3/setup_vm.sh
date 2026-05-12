b2c2309195f8659983b57fa9bb74f402bbb507bd
Arnab Kumar Ghosh <arnab@student-238-64.eduroam.uu.se>
2026-05-12 16:52:05 +0200

    ansible files

--- A3/setup_vm.sh
+++ A3/setup_vm.sh
@@ -8,6 +8,9 @@ sudo apt install python3-openstackclient
 sudo apt install python3-novaclient
 sudo apt install python3-keystoneclient
 sudo apt install git
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible
 
 git clone https://bdtiger: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@github.com/bdtiger/data-engineering-II.git
 source UPPMAX_2026_1-24_openrc.sh
